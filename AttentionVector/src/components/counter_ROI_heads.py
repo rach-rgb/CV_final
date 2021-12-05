597be4d4a78e7f886638a2139d37f893baece938
rach-rgb <seharhc@gmail.com>
2021-12-05 16:39:05 +0900

    add: counterpart components

---
+++ AttentionVector/src/components/counter_ROI_heads.py
@@ -0,0 +1,49 @@
+import torch
+from typing import Dict, List
+from detectron2.layers import ShapeSpec
+from detectron2.structures import Instances
+from detectron2.modeling import ROI_HEADS_REGISTRY, StandardROIHeads
+
+from counter_output_layers import CounterOutputLayer
+
+
+@ROI_HEADS_REGISTRY.register()
+class CounterROIHeads(StandardROIHeads):
+    def __init__(self, cfg, input_shape):
+        super().__init__(cfg, input_shape,
+                         box_predictor=CounterOutputLayer(cfg, self.predictor_input_shape(cfg, input_shape)))
+
+    # calculate input shape of box_predictor
+    # parameters: input_shape - input shape for RoI Heads
+    def predictor_input_shape(self, cfg, input_shape):
+        # build input_shape of box_predictor
+        in_features = cfg.MODEL.ROI_HEADS.IN_FEATURES
+        in_channels = [input_shape[f].channels for f in in_features]
+        # Check all channel counts are equal
+        assert len(set(in_channels)) == 1, in_channels
+        pooler_resolution = cfg.MODEL.ROI_BOX_HEAD.POOLER_RESOLUTION
+
+        # box_head output size
+        head_output = (in_channels[0], pooler_resolution, pooler_resolution)
+
+        num_conv = cfg.MODEL.ROI_BOX_HEAD.NUM_CONV
+        conv_dim = cfg.MODEL.ROI_BOX_HEAD.CONV_DIM
+        num_fc = cfg.MODEL.ROI_BOX_HEAD.NUM_FC
+        fc_dim = cfg.MODEL.ROI_BOX_HEAD.FC_DIM
+
+        dict_head = {
+            "conv_dims": [conv_dim] * num_conv,
+            "fc_dims": [fc_dim] * num_fc,
+        }
+
+        for k, conv_dim in enumerate(dict_head['conv_dims']):
+            head_output = (conv_dim, head_output[1], head_output[2])
+
+        for k, fc_dim in enumerate(dict_head['fc_dims']):
+            head_output = fc_dim
+
+        o = head_output
+        if isinstance(o, int):
+            return ShapeSpec(channels=o)
+        else:
+            return ShapeSpec(channels=o[0], height=o[1], width=o[2])
---
+++ AttentionVector/src/components/counter_output_layers.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+from typing import Dict, Union
+from detectron2.layers import ShapeSpec
+from detectron2.config import configurable
+from detectron2.modeling import FastRCNNOutputLayers
+
+
+class CounterOutputLayer(FastRCNNOutputLayers):
+    @configurable
+    def __init__(self, input_shape: ShapeSpec, *, box2box_transform, num_classes: int, test_score_thresh: float = 0.0,
+                 test_nms_thresh: float = 0.5, test_topk_per_image: int = 100, cls_agnostic_bbox_reg: bool = False,
+                 smooth_l1_beta: float = 0.0, box_reg_loss_type: str = "smooth_l1",
+                 loss_weight: Union[float, Dict[str, float]] = 1.0):
+
+        super().__init__(input_shape, box2box_transform=box2box_transform, num_classes=num_classes,
+                         test_score_thresh=test_score_thresh, test_nms_thresh=test_nms_thresh,
+                         test_topk_per_image=test_topk_per_image, cls_agnostic_bbox_reg=cls_agnostic_bbox_reg,
+                         smooth_l1_beta=smooth_l1_beta, box_reg_loss_type=box_reg_loss_type, loss_weight=loss_weight)
+
+        self.fc1 = nn.Linear(num_classes+1, 64)
+        self.fc2 = nn.Linear(64, num_classes+1)
+
+    def forward(self, x):
+        if x.dim() > 2:
+            x = torch.flatten(x, start_dim=1)
+        scores = self.cls_score(x)
+        scores = self.fc1(scores)
+        scores = self.fc2(scores)
+        scores = torch.sigmoid(scores)
+
+        proposal_deltas = self.bbox_pred(x)
+        return scores, proposal_deltas
